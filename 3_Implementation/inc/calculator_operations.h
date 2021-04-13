1ad36b6896aeedafad7a318c10698601192589d1
prasanna-chennamsetti <60134702+prasanna-chennamsetti@users.noreply.github.com>
2021-04-13 12:52:15 +0530

    Update calculator_operations.h

--- 3_Implementation/inc/calculator_operations.h
+++ 3_Implementation/inc/calculator_operations.h
@@ -9,39 +9,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio_ext.h>
+#include<math.h>
 
-/**
-*  adds the operand1 and operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 + operand2
-*/
-int add(int operand1, int operand2);
+void add(int a,int b)
+{
+	//Addition of elements
+	
+}
+void sub(int a,int b)
+{
+    
+    //Subtraction of elements
+}
+void mult(int a,int b)
+{
+    
+    //Multiplication of elements
+}
+void div(int a,int b)
+{
 
-/**
-*  subtracts the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 - operand2
-*/
-int subtract(int operand1, int operand2);
-
-/**
-*  multiply the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 * operand2
-*/
-int multiply(int operand1, int operand2);
-
-
-/**
-* divides the operand1 by operand2 and returns the result 
-* @param[in] operand1 
-* @param[in] operand2 
-* @return integer value of the operand1 / operand2
-* @note returns 0 for divide by 0 error
-*/
-int divide(int operand1, int operand2);
-
-#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
+   //Division of elements
+   
+}
+void power(int a,int b)
+{
+    //Power value of an element
+}
+void mod(int a,int b)
+{
+    //Modulus value of two values
+}
